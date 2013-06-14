de85348bfff9086eec8b4c13253c915eabd995af
Valentin Kaminskiy <valentin.kaminskiy@rightscale.com>
2013-06-14 15:50:13 +0300

    Failed method

--- lib/stqatools.rb
+++ lib/stqatools.rb
@@ -3,5 +3,11 @@ require 'stqatools/cloud_analyzer'
 
 module Stqatools
   # Your code goes here...
+  BIG_VAR = 'XTX'
   puts 'hello gem'
+  TEXT = "TEST"
+  
+  def Stqatools.met1
+    pust "method 1"
+  end
 end
